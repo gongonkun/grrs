eec220be7657b9d561e4df8364489fadab0b0778
sakata <922d.sakata@gmail.com>
2022-04-26 12:57:50 +0900

    add: arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use std::fmt::{Formatter, Display};
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser, Debug)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+impl Display for Cli {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        write!(f, "pattern: {}, path: {}", self.pattern, self.path.display())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("args = {}", args);
 }
